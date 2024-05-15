dedc6c384279ab84c43155ccb8f92beb5863d0ad
Yoss20 <118304980+Yoss20@users.noreply.github.com>
2024-05-14 23:26:20 -0500

    Termine header logearse

--- src/main/java/util/MySQLConexion.java
+++ src/main/java/util/MySQLConexion.java
@@ -5,7 +5,7 @@ public class MySQLConexion {
 		Connection con = null;
 		try {
                     Class.forName("com.mysql.cj.jdbc.Driver"); 
-		String url = "jdbc:mysql://localhost:3306/bdNotas"; 
+		String url = "jdbc:mysql://localhost:3306/bdnotas"; 
 		String usr = "root";
 		String psw = ""; 
 		con = DriverManager.getConnection(url,usr,psw); 
